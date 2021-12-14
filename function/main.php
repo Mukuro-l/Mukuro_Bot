a0d8b78d67ed8d05cfc21dd15232a82979266982
coldeggs <g2744602949@outlook.com>
2021-12-14 19:59:53 +0800

    Add files via upload

--- function/main.php
+++ function/main.php
@@ -1,1 +1,55 @@
+<?php
+//这是你开发功能的地方
 
+//判断$msg内容
+if ($msg=="你好"){
+$bots_msg_type="群聊";//设置发送的消息为群聊消息，其他类型有：主聊，私聊，回复私聊，回复
+
+$send_msg="hello, world";//设置发送的消息内容
+
+bot_api($host,$qun,$send_msg,$qq,$bots_msg_type,$msgid);//调用发送消息的api，参数说明：$host即为你的服务地址，$qun即为QQ群号，$qq即为QQ号，$send_msg即为发送的消息内容，$bots_msg_type即为发送消息类型，$msgid即为消息id
+
+}
+
+/*
+ *为开发者准备了内置的机器人配置函数的使用方法
+ *是否存在权限问题？不会。
+*/
+
+//判断关键词
+if ($msg=="开机"||$msg=="关机"){
+$bots_msg_type="回复";
+//读取配置文件
+$miui=file_get_contents($dir_qun,"r");
+//转为数组
+$miui=json_decode($miui,true);
+//判断状态
+if ($miui[$qq]==$msg){
+$send_msg="本来就是".$msg."状态";
+//调用api
+bot_api($host,$qun,$send_msg,$qq,$bots_msg_type,$msgid);
+}else{
+$bots_msg_type="回复";
+//识别动作类型
+$bb_type="开关机";
+//调用函数Switch_machine，该函数内置了部分开关控制
+$send_msg=Switch_machine($msg,$qq,$qun,$qhost,$bb_type);
+bot_api($host,$qun,$send_msg,$qq,$bots_msg_type,$msgid);
+}
+}
+
+//基本的状态判断
+/*
+//读取配置文件
+$miui=file_get_contents($dir_qun,"r");
+//转为数组
+$miui=json_decode($miui,true);
+//判断状态
+if ($miui[$qq]==$msg){
+$send_msg="本来就是".$msg."状态";
+//调用api
+bot_api($host,$qun,$send_msg,$qq,$bots_msg_type,$msgid);
+}
+
+*关于更多请自行查看json文件
+*/
