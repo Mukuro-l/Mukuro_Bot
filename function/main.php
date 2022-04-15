1a34eb0204030f807d12e5e8dbc4088d7d8716df
coldeggs <g2744602949@outlook.com>
2022-04-15 15:34:37 +0800

    Create main.php

---
+++ function/main.php
@@ -0,0 +1,12 @@
+<?php
+
+//第一阶段实验
+if ($msg == "你好"){
+$send_msg_type = "群聊";
+$send_msg = "你也好";
+$server_msg = new bot_msg_api();
+$server_msg->send($host,$qun,$send_msg,$qq,$bots_msg_type,$msgid);
+
+}
+
+?>
