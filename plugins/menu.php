f81a569cf1d78b045b291cc665ca36ce631bb235
coldeggs <g2744602949@outlook.com>
2022-04-17 04:49:55 +0800

    Update menu.php

--- plugins/menu.php
+++ plugins/menu.php
@@ -10,7 +10,7 @@ $url_qq="http://q.qlogo.cn/headimg_dl?dst_uin=".$qq."&spec=640&img_type=jpg";
 
 $send_msg = "[CQ:image,file=".$url_qq."]-----[菜单]-----\r\n测试菜单";
 
-$return_msg = $server_msg->send($Data,$send_type);
+$return_msg = $server_msg->send($Data,$send_type,$send_msg);
 
 echo "bot发送消息：[".$send_msg."]\n";
 
