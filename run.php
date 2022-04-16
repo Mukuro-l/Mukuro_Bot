89a302d689bbe0fa959e81f58bd1d7d61aae6398
coldeggs <g2744602949@outlook.com>
2022-04-17 03:34:02 +0800

    修复

--- run.php
+++ run.php
@@ -108,57 +108,54 @@ if(!empty($Data['group_id'])){
 
 //下方字段仅消息上报的字段
 
-@$msg=$Data['message']?:$_GET['msg'];//消息
+@$msg=$Data['message'];//消息
 
-@$real_msg=$Data['raw_message']?:$_GET['real_msg'];//真实消息
+@$real_msg=$Data['raw_message'];//真实消息
 
 @$qqinformation=$Data['sender'];
 
-@$qqnick=$qqinformation['nickname']?:$_GET['qqnick'];//昵称
+@$qqnick=$qqinformation['nickname'];//昵称
 
-@$qun=$Data['group_id']?:$_GET['qun'];//群号
+$GLOBALS["qun"]=$Data['group_id'];//群号
 
-@$qq=$Data['user_id']?:$_GET['qq'];//qq号
+$GLOBALS["qq"]=$Data['user_id'];//qq号
 
-@$qqadmin_get=$qqinformation['role']?:$_GET['qqadmin_get'];//群职位：admin/member
+@$qqadmin_get=$qqinformation['role'];//群职位：admin/member
 
-@$get_qqsex=$qqinformation['sex']?:$_GET['get_qqsex'];///male为男，female为女，unknown未知
+@$get_qqsex=$qqinformation['sex'];///male为男，female为女，unknown未知
 
 //══════api字段们═════＊/
 
 //══════事件监控字段们═════＊/
 
-@$get_qun_eve=$Data['notice_type']?:$_GET['get_qun_eve'];//事件
+@$get_qun_eve=$Data['notice_type'];//事件
 
-@$get_post_type=$Data['post_type']?:$_GET['get_post_type'];//获取上报类型
+@$get_post_type=$Data['post_type'];//获取上报类型
 
-@$get_tishi_api=$Data['sub_type']?:$_GET['get_tishi_api'];//获取提示类型
+@$get_tishi_api=$Data['sub_type'];//获取提示类型
 
-@$get_qing_api=$Data['request_type']?:$_GET['get_qing_api'];//获取请求类型
+@$get_qing_api=$Data['request_type'];//获取请求类型
 
-@$get_yanz_qun=$Data['comment']?:$_GET['get_yanz_qun'];//获取群验证消息
+@$get_yanz_qun=$Data['comment'];//获取群验证消息
 
-@$get_cao_qun=$Data['operator_id']?:$_GET['get_cao_qun'];//获取操作者qq
+@$get_cao_qun=$Data['operator_id'];//获取操作者qq
 
-@$qunry=$Data['honor_type']?:$_GET['qunry'];//获取荣耀类型
+@$qunry=$Data['honor_type'];//获取荣耀类型
 
-@$cheqq=$Data['operator_id']?:$_GET['cheqq'];//撤回操作qq
+@$cheqq=$Data['operator_id'];//撤回操作qq
 
-@$msgid=$Data['message_id']?:$_GET['msgid'];//消息id
+$GLOBALS["msgid"]=$Data['message_id'];//消息id
 
-@$real_msgid=$Data['real_id']?:$_GET['real_msgid'];//获取真实信息id
+@$real_msgid=$Data['real_id'];//获取真实信息id
 
-@$msg_type=$Data['message_type']?:$_GET['msg_type'];//消息类型
+@$msg_type=$Data['message_type'];//消息类型
 
-@$chuo_userid=$Data['target_id']?:$_GET['chuo_userid'];//被戳qq
+@$chuo_userid=$Data['target_id'];//被戳qq
 
 //══════事件监控字段们═════＊/
 
 @include_once './module/api.php';//机器人各类api模块
-
-//全局变量
-
-initialization();
+$GLOBALS["bots_msg_type"]=$msg_type;
 
 //循环载入插件
 
