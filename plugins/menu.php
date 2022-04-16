0d5e9fba67db7e38d9787be32ebb95abf6e24106
coldeggs <g2744602949@outlook.com>
2022-04-17 04:05:52 +0800

    Update menu.php

--- plugins/menu.php
+++ plugins/menu.php
@@ -10,7 +10,7 @@ $url_qq="http://q.qlogo.cn/headimg_dl?dst_uin=".$qq."&spec=640&img_type=jpg";
 
 $send_msg = "[CQ:image,file=".$url_qq."]-----[菜单]-----\r\n点歌+歌名🌻语音点歌+歌名\r\n抖音";
 
-$return_msg = $server_msg->send($send_msg);
+$return_msg = $server_msg->send();
   echo "bot发送消息：[".$send_msg."]\n";
 
 $ws->push($frame->fd, $return_msg);
