4ebe1eb2dc008fbb859d29d66890d7d9d95d48c0
coldeggs <g2744602949@outlook.com>
2022-04-16 23:20:19 +0800

    Create menu.php

--- plugins/menu.php
+++ plugins/menu.php
@@ -8,7 +8,7 @@ $bots_msg_type = $msg_type;
 
 $url_qq="http://q.qlogo.cn/headimg_dl?dst_uin=".$qq."&spec=640&img_type=jpg";
 
-$send_msg = "[CQ:image,file=".$url_qq."]-----[菜单]-----\r\n测试菜单";
+$send_msg = "[CQ:image,file=".$url_qq."]-----[菜单]-----\r\n点歌+歌名🌻语音点歌+歌名\r\n抖音";
 
 $return_msg = $server_msg->send($host,$qun,$send_msg,$qq,$bots_msg_type,$msgid);echo 'bot发送消息：['.$send_msg.']\n';
 
