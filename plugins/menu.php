009b4e5f297b3fe4c622f2be309aa233e8388b29
coldeggs <g2744602949@outlook.com>
2022-04-17 04:40:30 +0800

    Create menu.php

---
+++ plugins/menu.php
@@ -0,0 +1,21 @@
+<?php
+
+$server_msg = new bot_msg_api();
+
+if ($msg == "菜单"){
+
+$send_type = $msg_type;
+
+$url_qq="http://q.qlogo.cn/headimg_dl?dst_uin=".$qq."&spec=640&img_type=jpg";
+
+$send_msg = "[CQ:image,file=".$url_qq."]-----[菜单]-----\r\n测试菜单";
+
+$return_msg = $server_msg->send($Data,$send_type);
+
+echo "bot发送消息：[".$send_msg."]\n";
+
+$ws->push($frame->fd, $return_msg);
+
+}
+
+?>
