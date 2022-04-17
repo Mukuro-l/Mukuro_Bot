7f7fff5e85d81389511f2071c91e4c2f63ff07df
coldeggs <g2744602949@outlook.com>
2022-04-18 00:37:37 +0800

    增加了data输出，配置判断设置

--- module/config.php
+++ module/config.php
@@ -6,15 +6,23 @@ $port = "6700";
 
 //机器人QQ号
 
-$robot = "275566";
+$robot = "";
 
 //主人QQ，管理机器人
 
-$qhost = "865366";
+$qhost = "";
 
 //二级目录，直接填写文件夹名称
 
 $directory = "";
+
+//版本号
+
+$SDK = "1.1.4";
+
+//是否输出完整data
+
+$Return_Data = false;
 
 if ($robot == null&&$qhost == null){
 
@@ -23,6 +31,5 @@ echo "config.php未设置，启动失败。";
 exit;
 
 }
-$Welcome_to_use = "PHProbot已成功启动\n欢迎使用PHProbot\n项目地址：https://github.com/2744602949/PHProbot\nQQ邮箱：coldeggs@qq.com\nOutlook邮箱：g2744602949@outlook.com";
 
 ?>
