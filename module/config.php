be9777ce5e9197efcb856bc8d037c645dadd738c
coldeggs <g2744602949@outlook.com>
2022-04-18 15:32:36 +0800

    增加了屏蔽错误等级和检测更新的配置

--- module/config.php
+++ module/config.php
@@ -24,11 +24,34 @@ $SDK = "1.1.4";
 
 $Return_Data = false;
 
+//报告错误等级  报告用户触发的错误：E_USER_ERROR；不报告：E_ALL。请自行参考php.ini进行设置
+
+$Error_level = "E_USER_ERROR";
+
+//检查配置
 if ($robot == null&&$qhost == null){
 
 echo "config.php未设置，启动失败。";
 
 exit;
+
+}
+
+//检测更新
+
+if (file_exists("botsdk.txt")!=true){
+
+//新版本
+
+$new_SDK = file_get_contents("http://www.mtapda.com");
+
+if ($SDK != $new_SDK){
+
+echo "PHProbot检测到更新\n当前版本：".$SDK."\n最新版：".$new_SDK."\n";
+
+file_put_contents("botsdk.txt",$SDK);
+
+}
 
 }
 
