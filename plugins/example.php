f5bdc26d1021815dac3145a0c1b90f746d79fbfb
coldeggs <g2744602949@outlook.com>
2022-05-07 20:08:03 +0800

    优化

--- plugins/example.php
+++ plugins/example.php
@@ -2,15 +2,14 @@
 
 //该文件为PHProbot插件开发示例
 
-//创建一个消息类
-
-$Server_msg = new bot_msg_api();
+//创建一个消息类，115版本以前不支持命名空间
+use PHProbot\Api;
 
 //开始识别消息内容
 
 if ($msg == "测试"){
 
-//设置消息发送类型
+//设置消息发送类型，114版本可设置[群聊、私聊]
 
 //$msg_type 为自动识别的消息来源[群聊、私聊]
 
@@ -20,17 +19,9 @@ $S_type = $msg_type;
 
 $_msg = "Hello, World";
 
-//调用消息类 send函数
+//调用消息类 send函数，在115版本以前不支持
 
-$return_msg = $Server_msg -> send($qun,$_msg,$qq,$S_type,$msgid);
-
-//调用swoole提交
-
-$ws -> push($frame->fd, $return_msg);
-
-//返回
-
- echo "bot发送消息：[".$_msg."]\n";
+PHProbot\Api::send();
 
 }
 
