b6c81193dd590372e77f7eda98126cb8a354ae73
coldeggs <g2744602949@outlook.com>
2022-05-11 18:07:02 +0800

    fix bug

--- plugins/网易点歌.php
+++ plugins/网易点歌.php
@@ -4,7 +4,7 @@
 use Hahadu\ImageFactory\Config\Config;
 use Hahadu\ImageFactory\Kernel\Factory;
 use PHProbot\Api;
-$return=PHProbot\Api::MsgS($MsgS_Data=[$msg=>"点歌","data"=>$msg]);
+$return=PHProbot\Api::MsgS($MsgS_Data=["msg"=>"点歌","data"=>$msg]);
 if ($return==null){
  $Api_data = array(
 "qun"=>$qun,
