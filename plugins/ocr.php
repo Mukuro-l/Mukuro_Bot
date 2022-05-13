8958ac4b9344ef86aa995fba4f5fbd93ee5b5337
coldeggs <g2744602949@outlook.com>
2022-05-13 14:46:46 +0800

    OCR

---
+++ plugins/ocr.php
@@ -0,0 +1,19 @@
+<?php
+use PHProbot\Api;
+/*
+*OCR图片识别插件
+*/
+$return=PHProbot\Api::MsgS($MsgS_Data=["msg"=>"看图","data"=>$msg]);
+if ($return!=null){
+$data=PHProbot\Api::OCR($return);
+$Api_data = array(
+"qun"=>$qun,
+"qq"=>$qq,
+"msg"=>$data,
+"S_type"=>$msg_type,
+"msg_id"=>$msg_id
+);
+$data=PHProbot\Api::send($Api_data);
+$ws -> push($frame->fd, $data);
+}
+?>
