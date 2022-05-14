8a45925f1586f7d71d7fa9aa7f68575eb2dbcf4f
coldeggs <g2744602949@outlook.com>
2022-05-14 18:08:04 +0800

    增加
    新插件

---
+++ plugins/禁言.php
@@ -0,0 +1,79 @@
+<?php
+use PHProbot\Api;
+use PHProbot\GT;
+
+$return=PHProbot\Api::MsgS($MsgS_Data=["msg"=>"禁言","data"=>$msg]);
+//禁言#群号#QQ号#时间
+if ($return!=null){
+if (strstr("#",$return)==true){
+$return_data=explode("#",$return);
+if (count($return_data)==4){
+$set_array = [
+"qun"=>$return_data[1],
+//执行人
+"qq"=>$qq,
+//被禁言的QQ号
+"ban_user"=>$return_data[2],
+//禁言时间
+"time"=>$return_data[3]
+];
+if (PHProbot\GT::ban($set_array)=="OK"){
+$Api_data = array(
+"qun"=>$qun,
+"qq"=>$qq,
+"msg"=>"禁言成功",
+"S_type"=>$msg_type,
+"msg_id"=>$msg_id
+);
+$data=PHProbot\Api::send($Api_data);
+$ws -> push($frame->fd, $data);
+}
+}
+if (count($return_data)==3){
+$set_array = [
+"qun"=>$qun,
+//执行人
+"qq"=>$qq,
+//被禁言的QQ号
+"ban_user"=>$return_data[2],
+//禁言时间
+"time"=>$return_data[3]
+];
+if (PHProbot\GT::ban($set_array)=="OK"){
+$Api_data = array(
+"qun"=>$qun,
+"qq"=>$qq,
+"msg"=>"禁言成功",
+"S_type"=>$msg_type,
+"msg_id"=>$msg_id
+);
+$data=PHProbot\Api::send($Api_data);
+$ws -> push($frame->fd, $data);
+}
+}
+}
+if (strstr("[CQ:at,qq=",$return)==true&&$return!=null){
+$return_data=explode(",qq=",$return);
+$return_data1=explode("]",$return_data[1]);
+$set_array = [
+"qun"=>$qun,
+//执行人
+"qq"=>$qq,
+//被禁言的QQ号
+"ban_user"=>$return_data1[0],
+//禁言时间
+"time"=>$return_data1[1]
+];
+if (PHProbot\GT::ban($set_array)=="OK"){
+$Api_data = array(
+"qun"=>$qun,
+"qq"=>$qq,
+"msg"=>"禁言成功",
+"S_type"=>$msg_type,
+"msg_id"=>$msg_id
+);
+$data=PHProbot\Api::send($Api_data);
+$ws -> push($frame->fd, $data);
+}
+}
+}
