0c43a738e9016fa664c2aa3afec18ce9178eea0a
coldeggs <g2744602949@outlook.com>
2022-05-14 19:26:20 +0800

    fix bug
    修复无法禁言
    优化禁言识别

--- plugins/禁言.php
+++ plugins/禁言.php
@@ -57,6 +57,7 @@ $ws -> push($frame->fd, $data);
 $return_data=explode(",qq=",$return);
 $return_data1=explode("]",$return_data[1]);
 if (count($return_data1)==2){
+$return_time=str_replace(' ', '', $return_data[1]);
 $set_array = [
 "qun"=>$qun,
 //执行人
@@ -64,9 +65,18 @@ $set_array = [
 //被禁言的QQ号
 "ban_user"=>$return_data1[0],
 //禁言时间
-"time"=>$return_data1[1]
+"time"=>$return_time
 ];
 if (PHProbot\GT::ban($set_array)=="OK"){
+if ($return_time==0){
+$Api_data = array(
+"qun"=>$qun,
+"qq"=>$qq,
+"msg"=>"解禁成功",
+"S_type"=>$msg_type,
+"msg_id"=>$msg_id
+);
+}else{
 $Api_data = array(
 "qun"=>$qun,
 "qq"=>$qq,
@@ -74,6 +84,7 @@ $Api_data = array(
 "S_type"=>$msg_type,
 "msg_id"=>$msg_id
 );
+}
 $data=PHProbot\Api::send($Api_data);
 $ws -> push($frame->fd, $data);
 }
