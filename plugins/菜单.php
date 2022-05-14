ebc16baba4ec1aa02fbabec6f2586fe5817f77a7
coldeggs <g2744602949@outlook.com>
2022-05-14 12:24:17 +0800

    修改
    去掉公告\增加看图（ocr）

--- plugins/菜单.php
+++ plugins/菜单.php
@@ -1,11 +1,11 @@
 <?php
 use PHProbot\api;
-$url_qq="http://q.qlogo.cn/headimg_dl?dst_uin=".$qq."&spec=640&img_type=jpg";
+$url_qq="http://q.qlogo.cn/headimg_dl?dst_uin=".$qq."&spec=640";
 if (PHProbot\Api::MC($option=["菜单","手册","帮助"],$msg)==true){
 $Api_data = array(
 "qun"=>$qun,
 "qq"=>$qq,
-"msg"=>"[CQ:image,file=".$url_qq."]-----[菜单]-----\r\n点歌+歌名🌻语音点歌+歌名\r\n词典+词🐻色图\r\n小公告：".file_get_contents(".../root/sd/gg.txt"),
+"msg"=>"[CQ:image,file=".$url_qq."]-----[菜单]-----\r\n点歌+歌名🌻语音点歌+歌名\r\n词典+词🐻色图\r\n看图+图片",
 "S_type"=>$msg_type,
 "msg_id"=>$msg_id
 );
