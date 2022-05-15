598dd24ddd813da1d9dae9b7d0387da2b798dd4d
coldeggs <g2744602949@outlook.com>
2022-05-15 18:14:21 +0800

    优化
    增加权限

--- plugins/插件管理.php
+++ plugins/插件管理.php
@@ -1,6 +1,6 @@
 <?php
 //图文合成库
-use Hahadu\ImageFactory\Config\Config;
+//use Hahadu\ImageFactory\Config\Config;
 use Hahadu\ImageFactory\Kernel\Factory;
 use PHProbot\Api;
 if ($msg == "插件列表"){
@@ -14,8 +14,8 @@ file_put_contents("plugins_list.txt",$plugins_list,FILE_APPEND);
 }
 file_put_contents("plugins_list.txt","PS：开启|关闭+插件名 即可管理插件",FILE_APPEND);
 $text=file_get_contents("plugins_list.txt");
-$config->setSavePath="../gocq/data/images/";
-Factory::setOptions($config);
+//$config->setSavePath="../gocq/data/images/";
+//Factory::setOptions($config);
 $option=[
 'background'=>'#f5f5dc',
 'fill_color'=>'#000000',
@@ -42,6 +42,8 @@ unlink("plugins_list.txt");
 if (strstr($msg,"关闭")==true){
 $return=PHProbot\Api::MsgS($MsgS_Data=["msg"=>"关闭","data"=>$msg]);
 if ($return!=null){
+$BOT_Config =json_decode(file_get_contents("config.json"),true);
+if ($BOT_Config["qhost"]==$qq){
 $plugins_data = json_decode(file_get_contents("plugins_switch.json"),true);
 do{
 for ($i=0;$i<count($plugins_data);$i++){
@@ -62,12 +64,15 @@ $Api_data = array(
 $data=PHProbot\Api::send($Api_data);
 $ws -> push($frame->fd, $data);
 }
+}
 
 }
 
 if (strstr($msg,"开启")==true){
 $return=PHProbot\Api::MsgS($MsgS_Data=["msg"=>"开启","data"=>$msg]);
-if ($return!=null){ 
+if ($return!=null){
+$BOT_Config =json_decode(file_get_contents("config.json"),true);
+if ($BOT_Config["qhost"]==$qq){
 $plugins_data = json_decode(file_get_contents("plugins_switch.json"),true);
 do{
 for ($i=0;$i<count($plugins_data);$i++){
@@ -87,6 +92,7 @@ $Api_data = array(
 );
 $data=PHProbot\Api::send($Api_data);
 $ws -> push($frame->fd, $data);
+}
 }
 }
 ?>
