9e8ffbf06568361a210b48e4ae7f415c418a0005
coldeggs <g2744602949@outlook.com>
2022-05-25 17:43:24 +0800

    Add files via upload

--- module/api.php
+++ module/api.php
@@ -293,6 +293,44 @@ return "OK";
 }
 }
 
+
 }
+
+
+
+//带水印
+function To_wimage($file,$qq){
+if (!empty($file)){
+$image = $file;
+$config = new Config();
+$config->setSavePath = 'images/';
+$config->waterMarkText = 'PHProbot'; //设置水印文字，支持\n换行符
+$config->TextStyle = [
+'font_size' => 50, //字体大小
+];
+Factory::setOptions($config);$text_water_mark = Factory::text_to_image()->text_water_mark($image,$x='right',$y='down',$option=[]);
+copy($text_water_mark,"../gocq/data/images/".$qq.".jpg");
+unlink($text_water_mark);
+return "[CQ:image,file=".$qq.".jpg]";
+}
+}
+
+function To_image($text,$qq){
+
+if (!empty($text)){
+$option=[
+'background'=>'#f5f5dc',
+'fill_color'=>'#000000',
+'font_size'=>'20',
+'filename'=>$qq,
+'format'=>'jpg',
+];
+$text_mark_url = Factory::text_to_image()->text_create_image($text,$option);
+$file=$text_mark_url;
+
+return To_wimage($file,$qq);
+}
+}
+
 
 ?>
