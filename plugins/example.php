39c9f86ab67f224a5a60ec8140c8c9ae95de3bc2
coldeggs <g2744602949@outlook.com>
2022-05-25 17:44:07 +0800

    Add files via upload

--- plugins/example.php
+++ plugins/example.php
@@ -12,7 +12,6 @@ if ($msg == "test"){
 
 //多选
 if (PHProbot\Api::MC($option=["你好","测试"],$msg)==true){
-$image= new image();
 $text="你好";
 //设置数据组
 $Api_data = array(
@@ -21,7 +20,7 @@ $Api_data = array(
 //QQ号
 "qq"=>$qq,
 //msg为设置发送消息
-"msg"=>$image->To_image($text,$qq),
+"msg"=>To_image($text,$qq),
 //发送消息类型 $msg_type为消息类型，还可设置为[群聊，私聊]
 "S_type"=>$msg_type,
 //消息ID
