d37d686bb4f4903e4c4a0223c905d1611a5a4a79
coldeggs <g2744602949@outlook.com>
2022-05-25 16:07:31 +0800

    Add files via upload

--- run.php
+++ run.php
@@ -144,7 +144,41 @@ print_r($Data);
 
 //事件监控字段//
 
+//带水印
+function To_wimage($file,$qq){
+if (!empty($file)){
+$image = $file;
+$config = new Config();
+$config->setSavePath = 'images/';
+$config->waterMarkText = 'PHProbot'; //设置水印文字，支持\n换行符
+$config->TextStyle = [
+'font_size' => 50, //字体大小
+];
+Factory::setOptions($config);$text_water_mark = Factory::text_to_image()->text_water_mark($image,$x='right',$y='down',$option=[]);
+copy($text_water_mark,"../gocq/data/images/".$qq.".jpg");
+unlink($text_water_mark);
+return "[CQ:image,file=".$qq.".jpg]";
+}
+}
 
+
+function To_image($text,$qq){
+if (!empty($text)){
+$config->setSavePath="../gocq/data/images/";
+Factory::setOptions($config);
+$option=[
+'background'=>'#f5f5dc',
+'fill_color'=>'#000000',
+'font_size'=>'20',
+'filename'=>$qq,
+'format'=>'jpg',
+];
+$text_mark_url = Factory::text_to_image()->text_create_image($text,$option);
+$file=$text_mark_url;
+
+return To_wimage($file,$qq);
+}
+}
 
 
 include './module/config.php';//配置
@@ -196,45 +230,7 @@ $file=$file_array[$i]["插件名"];
 if ($file_array[$i]["状态"]=="开"){
 //echo "\n".$file."成功载入\n";
 include './plugins/'.$file;
-if (!function_exists("To_wimage")){
-//带水印
-function To_wimage($file,$qq){
-if (!empty($file)){
-$image = $file;
-$config = new Config();
-$config->setSavePath = 'images/';
-$config->waterMarkText = 'PHProbot'; //设置水印文字，支持\n换行符
-$config->TextStyle = [
-'font_size' => 50, //字体大小
-];
-Factory::setOptions($config);$text_water_mark = Factory::text_to_image()->text_water_mark($image,$x='right',$y='down',$option=[]);
-copy($text_water_mark,"../gocq/data/images/".$qq.".jpg");
-unlink($text_water_mark);
-return "[CQ:image,file=".$qq.".jpg]";
-}
-}
 
-}
-
-if (!function_exists("To_image")){
-function To_image($text,$qq){
-if (!empty($text)){
-$config->setSavePath="../gocq/data/images/";
-Factory::setOptions($config);
-$option=[
-'background'=>'#f5f5dc',
-'fill_color'=>'#000000',
-'font_size'=>'20',
-'filename'=>$qq,
-'format'=>'jpg',
-];
-$text_mark_url = Factory::text_to_image()->text_create_image($text,$option);
-$file=$text_mark_url;
-
-return To_wimage($file,$qq);
-}
-}
-}
 
 }
 }
