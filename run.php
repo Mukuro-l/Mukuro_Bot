dc4823165b829382139e37bb5a222cca11950814
coldeggs <g2744602949@outlook.com>
2022-05-25 15:55:17 +0800

    Add files via upload

--- run.php
+++ run.php
@@ -90,45 +90,7 @@ print_r($Data);
 }
 }
 
-if (!function_exists("To_wimage")){
-//带水印
-function To_wimage($file,$qq){
-if (!empty($file)){
-$image = $file;
-$config = new Config();
-$config->setSavePath = 'images/';
-$config->waterMarkText = 'PHProbot'; //设置水印文字，支持\n换行符
-$config->TextStyle = [
-'font_size' => 50, //字体大小
-];
-Factory::setOptions($config);$text_water_mark = Factory::text_to_image()->text_water_mark($image,$x='right',$y='down',$option=[]);
-copy($text_water_mark,"../gocq/data/images/".$qq.".jpg");
-unlink($text_water_mark);
-return "[CQ:image,file=".$qq.".jpg]";
-}
-}
 
-}
-
-if (!function_exists("To_image")){
-function To_image($text,$qq){
-if (!empty($text)){
-$config->setSavePath="../gocq/data/images/";
-Factory::setOptions($config);
-$option=[
-'background'=>'#f5f5dc',
-'fill_color'=>'#000000',
-'font_size'=>'20',
-'filename'=>$qq,
-'format'=>'jpg',
-];
-$text_mark_url = Factory::text_to_image()->text_create_image($text,$option);
-$file=$text_mark_url;
-
-return To_wimage($file,$qq);
-}
-}
-}
 
 //=============预定义函数==========
 
@@ -222,6 +184,45 @@ file_put_contents("plugins_switch.json",$plugins_list);
 
 }
 
+if (!function_exists("To_wimage")){
+//带水印
+function To_wimage($file,$qq){
+if (!empty($file)){
+$image = $file;
+$config = new Config();
+$config->setSavePath = 'images/';
+$config->waterMarkText = 'PHProbot'; //设置水印文字，支持\n换行符
+$config->TextStyle = [
+'font_size' => 50, //字体大小
+];
+Factory::setOptions($config);$text_water_mark = Factory::text_to_image()->text_water_mark($image,$x='right',$y='down',$option=[]);
+copy($text_water_mark,"../gocq/data/images/".$qq.".jpg");
+unlink($text_water_mark);
+return "[CQ:image,file=".$qq.".jpg]";
+}
+}
+
+}
+
+if (!function_exists("To_image")){
+function To_image($text,$qq){
+if (!empty($text)){
+$config->setSavePath="../gocq/data/images/";
+Factory::setOptions($config);
+$option=[
+'background'=>'#f5f5dc',
+'fill_color'=>'#000000',
+'font_size'=>'20',
+'filename'=>$qq,
+'format'=>'jpg',
+];
+$text_mark_url = Factory::text_to_image()->text_create_image($text,$option);
+$file=$text_mark_url;
+
+return To_wimage($file,$qq);
+}
+}
+}
 //===============分割============
 
 for ($i=0;$i<count($list);$i++){
