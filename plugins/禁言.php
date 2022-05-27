ce158f2b0d6da087c1b3944016f10dd4f9c7a7e4
coldeggs <g2744602949@outlook.com>
2022-05-27 20:00:50 +0800

    Update 禁言.php

--- plugins/禁言.php
+++ plugins/禁言.php
@@ -24,7 +24,7 @@ $Api_data = array(
 "S_type"=>$msg_type,
 "msg_id"=>$msg_id
 );
-$data=PHProbot\Api::send($Api_data);
+$Return_data=PHProbot\Api::send($Api_data);
 }
 }
 if (count($return_data)==3){
@@ -45,7 +45,7 @@ $Api_data = array(
 "S_type"=>$msg_type,
 "msg_id"=>$msg_id
 );
-$data=PHProbot\Api::send($Api_data);
+$Return_data=PHProbot\Api::send($Api_data);
 }
 }
 
@@ -80,7 +80,7 @@ $Api_data = array(
 "msg_id"=>$msg_id
 );
 }
-$data=PHProbot\Api::send($Api_data);
+$Return_data=PHProbot\Api::send($Api_data);
 }
 }
 }
