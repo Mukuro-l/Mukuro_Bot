95fd795dcf6b52346158cb2d219d3a0d3bfafa89
coldeggs <g2744602949@outlook.com>
2022-06-07 16:52:44 +0800

    Add files via upload

---
+++ vendor/hahadu/image-factory/src/Kernel/Models/ImagickModel.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *  +----------------------------------------------------------------------
+ *  | Created by  hahadu (a low phper and coolephp)
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2020. [hahadu] All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | SiteUrl: https://github.com/hahadu/wechat
+ *  +----------------------------------------------------------------------
+ *  | Author: hahadu <582167246@qq.com>
+ *  +----------------------------------------------------------------------
+ *  | Date: 2020/10/29 下午9:34
+ *  +----------------------------------------------------------------------
+ *  | Description:   ImagickModel
+ *  +----------------------------------------------------------------------
+ **/
+
+namespace Hahadu\ImageFactory\Kernel\Models;
+
+class ImagickModel
+{
+    private $imagick;
+    public function __construct($imagick){
+        $this->imagick = $imagick;
+    }
+    public function set_images($style){
+
+    }
+    public function add_image(){
+
+    }
+
+}
--- vendor/hahadu/image-factory/src/Kernel/Models/PositionModel.php
+++ vendor/hahadu/image-factory/src/Kernel/Models/PositionModel.php
@@ -1,1 +1,48 @@
-1
+<?php
+/**
+ *  +----------------------------------------------------------------------
+ *  | Created by  hahadu (a low phper and coolephp)
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2020. [hahadu] All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | SiteUrl: https://github.com/hahadu/wechat
+ *  +----------------------------------------------------------------------
+ *  | Author: hahadu <582167246@qq.com>
+ *  +----------------------------------------------------------------------
+ *  | Date: 2020/10/29 下午7:42
+ *  +----------------------------------------------------------------------
+ *  | Description:   position
+ *  +----------------------------------------------------------------------
+ **/
+
+namespace Hahadu\ImageFactory\Kernel\Models;
+
+
+class PositionModel
+{
+    public function text_right($image_width,$text_len,$font_size){
+        return $image_width-($text_len+1)*$font_size; //出血位补偿
+    }
+    public function text_center_x($image_width,$text_len,$font_size){
+        return ($image_width-$text_len*$font_size)/2;
+    }
+    public function text_center_y($image_width,$text_line,$font_size){
+        return ($image_width-$text_line*1.2*$font_size)/2;
+    }
+    public function text_down($image_height,$text_line,$font_size){
+        return $image_height-$text_line*1.2*$font_size;
+    }
+    public function image_center_x($image_width,$min_width){
+        return ($image_width-$min_width)/2;
+    }
+    public function image_center_y($image_width,$min_width){
+        return ($image_width-$min_width)/2;
+    }
+    public function image_down($image_height,$min_height){
+        return $image_height-$min_height;
+    }
+    public function image_right($image_width,$min_width){
+        return $image_width-$min_width;
+    }
+
+}
