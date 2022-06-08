3797a002caadb4c5cae636897014479eb3955063
coldeggs <g2744602949@outlook.com>
2022-06-08 14:57:17 +0800

    Add files via upload

--- vendor/hahadu/image-factory/src/Kernel/Models/Traits/ImagickTrait.php
+++ vendor/hahadu/image-factory/src/Kernel/Models/Traits/ImagickTrait.php
@@ -1,1 +1,31 @@
-1
+<?php
+/**
+ *  +----------------------------------------------------------------------
+ *  | Created by  hahadu (a low phper and coolephp)
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2020. [hahadu] All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | SiteUrl: https://github.com/hahadu/wechat
+ *  +----------------------------------------------------------------------
+ *  | Author: hahadu <582167246@qq.com>
+ *  +----------------------------------------------------------------------
+ *  | Date: 2020/10/30 下午7:14
+ *  +----------------------------------------------------------------------
+ *  | Description:   ImagickTrait
+ *  +----------------------------------------------------------------------
+ **/
+
+namespace Hahadu\ImageFactory\Kernel\Models\Traits;
+
+
+trait ImagickTrait
+{
+    /*****
+     * @param \Imagick $imagick
+     * @return \Imagick
+     */
+    public function set_image(& $imagick,$style=[]){
+       return $imagick;
+    }
+
+}
