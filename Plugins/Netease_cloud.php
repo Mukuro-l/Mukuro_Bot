294ea24e8fac17b60ab43f6e201ce3721b6c3cf2
coldeggs <g2744602949@outlook.com>
2022-06-08 08:27:01 +0800

    更新
    增加选歌，并优化

--- Plugins/Netease_cloud.php
+++ Plugins/Netease_cloud.php
@@ -10,23 +10,16 @@ use Api;
 function plugins_Netease_cloud(){
 $return=$this->MsgS(["msg"=>"点歌","data"=>$this->msg]);
 if ($return!=null){
-var_dump($return);
 
-$str="https://autumnfish.cn/search?keywords=$return";
+$url="https://autumnfish.cn/search?keywords=".urlencode($return);
 
-$str=$this->Curl($str);
+$song_data=json_decode(file_get_contents($url),true);
+$result=$song_data['result'];
+$song_list=$result['songs'];//歌曲列表
+$list_data=$song_list[$return_list[0]-1];//选歌
+$song_id = $list_data["id"];
 
-$str=json_decode($str,true);
-
-$str=$str['result'];
-
-$str=$str['songs'];//歌曲列表
-
-$ga1=$str[0];//选歌
-
-$id=$ga1['id'];
-
-if ($id==null){
+if ($song_id==null){
 return $this->send("获取失败");
 
 }else{
@@ -52,8 +45,51 @@ Text_Images($text, $qq);
 return $this->send("[CQ:image,file=".$this->qq.".jpg]");
 }
 
+}
+if (preg_match("/^[0-9]+$/u", $this->msg, $return_list)){
+
+if (is_file($qq."song_list.txt")==true){
+
+$song=file_get_contents($qq."song.txt");
+$data_one = explode("#",$song);
+$data1 = $data_one[0];
+if ($data1 == "语音"){
+$data2 = $data_one[1];
+$url="https://autumnfish.cn/search?keywords=".urlencode($data2);
+$song_data=json_decode(file_get_contents($url),true);
+$result=$song_data['result'];
+$song_list=$result['songs'];//歌曲列表
+$list_data=$song_list[$return_list[0]-1];//选歌
+$song_id = $list_data["id"];
+$url = "http://music.163.com/song/media/outer/url?id=".$song_id.".mp3";
+$url_data = file_get_contents($url);
+file_put_contents("../gocq/data/voices/".$song_id.".mp3",$url_data);
+
+$this->send("[CQ:record,file=".$song_id.".mp3]");
+unlink($qq."song_list.txt");
+}else{
+$song=file_get_contents($qq."song.txt");
+$data_one = explode("#",$song);
+$data1 = $data_one[0];
+$data2 = $data_one[1];
+
+$url="https://autumnfish.cn/search?keywords=".urlencode($data2);
+$song_data=json_decode(file_get_contents($url),true);
+$result=$song_data['result'];
+$song_list=$result['songs'];//歌曲列表
+$list_data=$song_list[$return_list[0]-1];//选歌
+$song_id = $list_data["id"];
+$this->send("[CQ:music,type=163,id=".$song_id."]");
+
+unlink($qq."song_list.txt");
+
+unlink($qq."song.txt");
 
 }
+}
+
+}
+
 }
 }
 ?>
