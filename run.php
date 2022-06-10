2f55dd5b9906a3bf9c7120cdeec483b136f6624d
Mukuro <g2744602949@outlook.com>
2022-06-10 19:23:26 +0800

    fix bug
    修复屏蔽问题

--- run.php
+++ run.php
@@ -8,14 +8,12 @@
  */
 define("Mukuro", "版权归coldeggs所有2021-2022。");
 define("E_mail", "phprobot@sina.com");
-
-if (is_dir("vendor")){
-include "./vendor/autoload.php";
-}else{
-echo "缺少必要的库，请阅读README.md文件\n";
-exit;
+if (is_dir("vendor")) {
+	include "./vendor/autoload.php";
+} else {
+	echo "缺少必要的库，请阅读README.md文件\n";
+	exit;
 }
-
 include './Module/Config.php';
 /*
 屏蔽错误
@@ -32,7 +30,6 @@ use Swoole\Coroutine\Barrier;
 use Swoole\Coroutine\System;
 use function Swoole\Coroutine\run;
 use Swoole\Coroutine;
-
 echo "Mukuro WebSocket服务器已启动，正在等待go-cqhttp连接……\n";
 //监听WebSocket连接打开事件
 $ws->on('Open', function ($ws, $request) {
@@ -42,10 +39,8 @@ $ws->on('Open', function ($ws, $request) {
 	echo $Welcome_to_use;
 	unlink("./Doc/Mukuro_Menu_Doc/Menu.doc");
 });
-
 //监听WebSocket消息事件
-$ws->on('Message', function ($ws, $frame) use ($database,$BOT_Config) {
-
+$ws->on('Message', function ($ws, $frame) use ($database, $BOT_Config) {
 	//fd为客户端标识, $ws调用push函数发送(第二个参数为消息内容)
 	$Data = $frame->data;
 	//json转为PHP数组，必须转为PHP对象
@@ -56,93 +51,114 @@ $ws->on('Message', function ($ws, $frame) use ($database,$BOT_Config) {
 			print_r($Data);
 		}
 	}
-	if (@$Data['meta_event_type'] !== 'heartbeat'||@$Data['meta_event_type'] !=='lifecycle') {
-	if (@$Data['status']===null){
-$Barrier = Barrier::make();
-go(function () use($Data,$ws,$frame,$database,$BOT_Config){
-	include_once './Module/Api.php';
-	include_once './Module/Function.php';
-	//这里会载入Plugins文件夹下的所有插件 115版本增加是否载入
-	$list = glob('./Plugins/*.php');
-	if (is_file("Plugins_switch.json") == false) {
-		$Plugins_array = array();
-		for ($i = 0;$i < count($list);$i++) {
-			$file = explode('/', $list[$i]) [2];
-			$Plugins_array[] = array("插件名" => $file, "状态" => "开");
-		}
-		$Plugins_list = json_encode($Plugins_array, JSON_UNESCAPED_UNICODE);
-		file_put_contents("Plugins_switch.json", $Plugins_list);
-	} else {
-		$file_array = json_decode(file_get_contents("Plugins_switch.json"), true);
-		//判断是否增加
-		if (count($list) > count($file_array)) {
-			for ($i = 0;$i < count($list);$i++) {
-				$file = explode('/', $list[$i]) [2];
-				if ($file_array[$i]["插件名"] != $file) {
-					unlink("Plugins_switch.json");
-				}
-			}
-		}
-		if (count($list) < count($file_array)) {
-			for ($i = 0;$i < count($list);$i++) {
-				$file = explode('/', $list[$i]) [2];
-				if ($file_array[$i]["插件名"] != $file) {
-					unlink("Plugins_switch.json");
-				}
-			}
-		}
-		for ($i=0;$i<count($list);$i++){
-
-$file=$file_array[$i]["插件名"];
-
-if ($file_array[$i]["状态"]=="开"){
-				$Plugins_name = explode('.', $file);
-				$Plugins_name = $Plugins_name[0];
-				@include_once './Plugins/' . $file;
-				if (!is_dir("./Doc/".$Plugins_name)){
-				mkdir("./Doc/".$Plugins_name);
-				$Doc = new ReflectionClass($Plugins_name);
-                $Doc=$Doc->getDocComment();
-                $Doc_doc_ = explode("*",$Doc);
-                //名字会出现在菜单上
-                $Doc_name = explode("@name",$Doc_doc_[3]);
-                $Doc_doc = explode("@doc",$Doc_doc_[4]);
-                $Doc_comment = explode("@comment",$Doc_doc_[5]);
-                $Doc_return = explode("@return",$Doc_doc_[6]);
-                $Doc_data ="    Mukuro  --".$Plugins_name."插件帮助\r\n名字：[".$Doc_name[1]."]\r\n详情：[".$Doc_doc[1]."]\r\n指令：[".$Doc_comment[1]."]\r\n返回：[".$Doc_return[1]."]";
-                file_put_contents("./Doc/".$Plugins_name."/".$Plugins_name.".doc",$Doc_data);
-				}
-				
-					$Plugins_name_function = "plugins_" . $Plugins_name;
-					$Plugins_test = new $Plugins_name($Data,$database,$BOT_Config);
-					$Plugins_return =  $Plugins_test->$Plugins_name_function();
-					//$Plugins_test -> __destruct();
-if (isset($Plugins_return)){
-					$ws->push($frame->fd, $Plugins_return);
+	if (@$Data['meta_event_type'] !== 'heartbeat' && @$Data['meta_event_type'] !== 'lifecycle') {
+		if (@$Data['status'] === null) {
+		
+			$Barrier = Barrier::make();
+			go(function () use ($Data, $ws, $frame, $database, $BOT_Config) {
+				include_once './Module/Api.php';
+				include_once './Module/Function.php';
+				//这里会载入Plugins文件夹下的所有插件 115版本增加是否载入
+				$list = glob('./Plugins/*.php');
+				if (is_file("Plugins_switch.json") == false) {
+					$Plugins_array = array();
+					for ($i = 0;$i < count($list);$i++) {
+						$file = explode('/', $list[$i]) [2];
+						$Plugins_array[] = array("插件名" => $file, "状态" => "开");
 					}
-					if (!is_file("./Doc/Mukuro_Menu_Doc/Menu.doc")){
-for ($i=0;$i<count($list);$i++){
-$Menu_doc=explode('.',$file_array[$i]["插件名"]);
-$doc_data=file_get_contents("./Doc/".$Menu_doc[0]."/".$Menu_doc[0].".doc");
-$doc_name = explode("]",$doc_data);
-$doc_name = explode("[",$doc_name[0]);
-
-file_put_contents("./Doc/Mukuro_Menu_Doc/Menu.doc",($i+1).$doc_name[1]."\r\n",FILE_APPEND);
-}
-				}
+					$Plugins_list = json_encode($Plugins_array, JSON_UNESCAPED_UNICODE);
+					file_put_contents("Plugins_switch.json", $Plugins_list);
+				} else {
+					$file_array = json_decode(file_get_contents("Plugins_switch.json"), true);
+					//判断是否增加
+					if (count($list) > count($file_array)) {
+						for ($i = 0;$i < count($list);$i++) {
+							$file = explode('/', $list[$i]) [2];
+							if ($file_array[$i]["插件名"] != $file) {
+								unlink("Plugins_switch.json");
+							}
+						}
+					}
+					if (count($list) < count($file_array)) {
+						for ($i = 0;$i < count($list);$i++) {
+							$file = explode('/', $list[$i]) [2];
+							if ($file_array[$i]["插件名"] != $file) {
+								unlink("Plugins_switch.json");
+							}
+						}
+					}
+					
+					
+					if (!is_file("./Group/".$Data['group_id']."/config.json")&&!empty($Data['group_id'])){
+					mkdir("./Group/".$Data['group_id']);
+					$Group_data = ["status"=>"关"];
+					file_put_contents("./Group/".$Data['group_id']."/config.json",json_encode($Group_data, JSON_UNESCAPED_UNICODE));
 					
 					}
+					
+					if ($BOT_Config["qhost"] === $Data['user_id']){
+					if (preg_match("/^封解主· ?(.*)\$/", $Data['message'], $Jiezhu)) {
+					if ($Jiezhu[1] === "开" || $Jiezhu[1] === "闭" ){
+					if ($Jiezhu[1] === "开"){
+					$instruction = "打开了哟";
+					}else{
+					$instruction = "关闭了哟";
+					}
+					$Jiezhu_data=json_decode(file_get_contents("./Group/".$Data['group_id']."/config.json"),true);
+					$Jiezhu_data["status"]=$Jiezhu[1];
+					file_put_contents("./Group/".$Data['group_id']."/config.json",json_encode($Jiezhu_data, JSON_UNESCAPED_UNICODE));
+					
+					$url = ["action" => "send_group_msg", "params" => ["group_id" => $Data['group_id'], "message" =>"官人～六儿把群[".$Data['group_id']."]".$instruction."了哟😘" ]];
+				$submit_data = json_encode($url, JSON_UNESCAPED_UNICODE);
+				$ws->push($frame->fd, $submit_data);
 				}
-			
+				}
+				}
+					
+					if (json_decode(file_get_contents("./Group/".$Data['group_id']."/config.json"),true)["status"] === "开" || $Data['group_id'] === null){
+					for ($i = 0;$i < count($list);$i++) {
+						$file = $file_array[$i]["插件名"];
+						if ($file_array[$i]["状态"] == "开") {
+							$Plugins_name = explode('.', $file);
+							$Plugins_name = $Plugins_name[0];
+							@include_once './Plugins/' . $file;
+							if (!is_dir("./Doc/" . $Plugins_name)) {
+								mkdir("./Doc/" . $Plugins_name);
+								$Doc = new ReflectionClass($Plugins_name);
+								$Doc = $Doc->getDocComment();
+								$Doc_doc_ = explode("*", $Doc);
+								//名字会出现在菜单上
+								$Doc_name = explode("@name", $Doc_doc_[3]);
+								$Doc_doc = explode("@doc", $Doc_doc_[4]);
+								$Doc_comment = explode("@comment", $Doc_doc_[5]);
+								$Doc_return = explode("@return", $Doc_doc_[6]);
+								$Doc_data = "    Mukuro  --" . $Plugins_name . "插件帮助\r\n名字：[" . $Doc_name[1] . "]\r\n详情：[" . $Doc_doc[1] . "]\r\n指令：[" . $Doc_comment[1] . "]\r\n返回：[" . $Doc_return[1] . "]";
+								file_put_contents("./Doc/" . $Plugins_name . "/" . $Plugins_name . ".doc", $Doc_data);
+							}
+							$Plugins_name_function = "plugins_" . $Plugins_name;
+							$Plugins_test = new $Plugins_name($Data, $database, $BOT_Config);
+							$Plugins_return = $Plugins_test->$Plugins_name_function();
+							//$Plugins_test -> __destruct();
+							if (isset($Plugins_return)) {
+								$ws->push($frame->fd, $Plugins_return);
+							}
+							if (!is_file("./Doc/Mukuro_Menu_Doc/Menu.doc")) {
+								for ($i = 0;$i < count($list);$i++) {
+									$Menu_doc = explode('.', $file_array[$i]["插件名"]);
+									$doc_data = file_get_contents("./Doc/" . $Menu_doc[0] . "/" . $Menu_doc[0] . ".doc");
+									$doc_name = explode("]", $doc_data);
+									$doc_name = explode("[", $doc_name[0]);
+									file_put_contents("./Doc/Mukuro_Menu_Doc/Menu.doc", ($i + 1) . $doc_name[1] . "\r\n", FILE_APPEND);
+								}
+							}
+						}
+					}
+				  }
+				}
+			});
+			Barrier::wait($Barrier);
 		}
-	
-});
-Barrier::wait($Barrier);
-
-}
-}	
-	
-
+	}
 	//定时器逻辑
 	if ($BOT_Config["_tick"] == true) {
 		if (file_exists("tick_config.json") == true) {
@@ -184,7 +200,6 @@ Barrier::wait($Barrier);
 			});
 		}
 	}
-	
 });
 //监听WebSocket连接关闭事件
 @$ws->on('Close', function ($ws, $fd) use ($the_tick) {
