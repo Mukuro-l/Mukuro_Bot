550c6b4d08af145654d16096c423297e8e54d1c9
Mukuro <g2744602949@outlook.com>
2022-06-18 15:50:17 +0800

    Update Meme.php

--- Plugins/Meme.php
+++ Plugins/Meme.php
@@ -4,13 +4,14 @@ use Mukuro\Module\Api;
 /**
 *@name 头像表情包生成
 *@doc 生成QQ头像有趣表情包，现在支持：一分钟
-*@comment 一分钟
+*@comment 一分钟at某人 or 一分钟自己
 *@return image
 */
 class Meme{
 use Api;
   function plugins_Meme(){
-    if ($data=$this->MsgS(["msg"=>"一分钟", "data"=>$this->msg])!= null){
+    $data=$this->MsgS(["msg"=>"一分钟", "data"=>$this->msg])
+    if ($data!= null){
       if ($data=="自己"){
     return $this->send(HeiHei("一分钟", $this->qq));
         }else{
