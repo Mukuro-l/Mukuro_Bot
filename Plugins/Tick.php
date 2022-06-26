afaf53cdecbb8b2ec5321d8bba3f5a5986d5fd5c
Mukuro <g2744602949@outlook.com>
2022-06-26 17:53:17 +0800

    更新
    更改了判断方式

--- Plugins/Tick.php
+++ Plugins/Tick.php
@@ -6,29 +6,24 @@ use Mukuro\Module\Api;
 /**
 *@name 定时任务
 *@doc 配置Mukuro定时任务的插件
-*@comment 添加定时任务#08:00:10#QQ号#群号#你好#次数（数字）
+*@comment 添加定时任务#080010#QQ号#群号#你好#次数（数字）
 *@return text
 */
 class Tick{
 use Api;
 function plugins_Tick(){
-$return=$this->MsgS($MsgS_Data=["msg"=>"添加定时任务","data"=>$this->msg]);
-if ($return!=null){
-$tickdata = explode("#",$return);
-if (count($tickdata)!=6){
-return $this->send("请按照要求设置！\r\n格式为[添加定时任务#080010#QQ号#群号#你好#次数（数字）]");
 
-}else{
+if (preg_match("/^添加定时任务# ?([0-9]+.*)# ?([0-9]+.*)# ?([0-9]+.*)# ?(.*)# ?([0-9]+.*)\$/", $this->msg, $return)) {
 $BOT_Config =json_decode(file_get_contents("config.json"),true);
 //判断是否有配置文件
 if (file_exists("tick_config.json")!=true){
 //写入第一次的定时任务
 $tick_array[$tickdata[1]][] = [
-"qq"=>$tickdata[2],
-"qun"=>$tickdata[3],
-"time"=>$tickdata[1],
-"tick"=>$tickdata[5],
-"msg"=>$tickdata[4],
+"qq"=>$return[2],
+"qun"=>$return[3],
+"time"=>$return[1],
+"tick"=>$return[5],
+"msg"=>$return[4],
 "http_port"=>$BOT_Config["http_port"]
 ];
 $data =json_encode($tick_array,JSON_UNESCAPED_UNICODE);
@@ -38,11 +33,11 @@ return $this->send("设置成功");
 $tick_array = json_decode(file_get_contents("tick_config.json"),true);
 if ($tick_array[$tickdata[1]]!=null){
 $tick_array[$tickdata[1]][count($tick_array[$tickdata[1]])] = [
-"qq"=>$tickdata[2],
-"qun"=>$tickdata[3],
-"time"=>$tickdata[1],
-"tick"=>$tickdata[5],
-"msg"=>$tickdata[4],
+"qq"=>$return[2],
+"qun"=>$return[3],
+"time"=>$return[1],
+"tick"=>$return[5],
+"msg"=>$return[4],
 "http_port"=>$BOT_Config["http_port"]
 ];
 $data =json_encode($tick_array,JSON_UNESCAPED_UNICODE);
@@ -50,11 +45,11 @@ file_put_contents("tick_config.json",$data);
 return $this->send("设置成功");
 }else{
 $tick_array[$tickdata[1]][] = [
-"qq"=>$tickdata[2],
-"qun"=>$tickdata[3],
-"time"=>$tickdata[1],
-"tick"=>$tickdata[5],
-"msg"=>$tickdata[4],
+"qq"=>$return[2],
+"qun"=>$return[3],
+"time"=>$return[1],
+"tick"=>$return[5],
+"msg"=>$return[4],
 "http_port"=>$BOT_Config["http_port"]
 ];
 $data =json_encode($tick_array,JSON_UNESCAPED_UNICODE);
@@ -62,8 +57,11 @@ file_put_contents("tick_config.json",$data);
 return $this->send("设置成功");
 }
 }
+
+}else if (strstr("添加定时任务#",$this->msg)){
+return $this->send("请按照要求设置！\r\n格式为[添加定时任务#080010#QQ号#群号#你好#次数（数字）]").$this->send("Σ(ŎдŎ|||)ﾉﾉ");
 }
-}
+
 }
 }
 ?>
