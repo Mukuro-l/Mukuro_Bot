c02d30e746051d65002b6f654114066d458cfc3d
Mukuro <g2744602949@outlook.com>
2022-09-03 03:48:11 +0800

    update
    自动部署go-cqhttp，仍在测试勿用

---
+++ install.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import re
+import os
+import subprocess
+#import pyyaml
+
+#这是一个Mukuro_Bot配置辅助生成程序
+print("你好，欢迎使用Mukuro_Bot开发框架")
+print("------------------------------")
+print("1.该框架并不会窃取你的个人信息")
+print("2.绝不会窃取个人账号信息")
+print("3.你使用本程序即代表着同意开源协议")
+print("------------------------------")
+select = input("是否继续？请选择(y|n):")
+while True:
+    if select == "y":
+        the_ad = subprocess.getoutput("uname -m")
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        if the_ad == "aarch64":
+            print("检测到系统为linux arm64")
+            print("正在下载对应版本的go-cqhttp")
+            subprocess.getoutput("sudo wget https://cdn.githubjs.cf/Mrs4s/go-cqhttp/releases/download/v1.0.0-rc3/go-cqhttp_linux_arm64.tar.gz")
+            print("已成功下载")
+            subprocess.getoutput("sudo mkdir "+str(os.path.dirname(file_path))+"/gocq/")
+            print("在上级文件夹创建目录：gocq")
+            subprocess.getoutput("sudo mv go-cqhttp_linux_arm64.tar.gz "+str(os.path.dirname(file_path))+"/gocq/")
+            subprocess.getoutput("cd "+str(os.path.dirname(file_path))+"/gocq/ && sudo tar -xzf go-cqhttp_linux_arm64.tar.gz && sudo rm go-cqhttp_linux_arm64.tar.gz")
+            print("已成功下载并解压go-cqhttp")
+            print("go-cqhttp所在文件夹："+str(os.path.dirname(file_path))+"/gocq/")
+            QQ = input("请输入你的机器人账号：")
+            data = re.search(r"[0-9]+",QQ,re.M|re.I)
+            if data != None:
+                password = input("请输入密码：")
+                user_exit = input("输入exit退出:")
+            else:
+                print("error:账号匹配出错！")
+                break
+
+                if user_exit == "exit":
+                    break
+
+
