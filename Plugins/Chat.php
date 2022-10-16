0e071608e0b902f1155787946c5889e6b2ce0c5c
Mukuro <g2744602949@outlook.com>
2022-10-17 01:49:35 +0800

    Add files via upload

--- Plugins/Chat.php
+++ Plugins/Chat.php
@@ -11,22 +11,13 @@ class Chat{
       function join_group(int $group_id,int $source){
       $this->Rsend("你可以在15秒内发送[取消]来退出\r\n或者在建立任务之后发送[取消]\r\n若15秒之内无任何响应，程序将会退出进程");
     
-      $begin1 = $this->context("跨群聊天".$group_id,$source);
+      $begin1 = $this->context("跨群聊天".$group_id,$source,0);
       if ($begin1[2]=="取消"){
       $this->Rsend("已取消");
       return;
       }else if (!empty($begin1[2])){
-      print_r($begin1);
       $this->send(["send_group_msg",$source,"已建立任务，并将消息发送对应群聊"]);
       $this->send(["send_group_msg",$group_id,"来自群聊[$source]的\r\n[$this->qq]说：\r\n[".$begin1[2]."]\r\nPS：你可以通过跨群聊天功能来回复"]);
-      /*$begin = $this->context($begin[2]);
-      if ($begin[2]=="取消"){
-      return;
-      }else{
-      
-      $this->send(["send_group_msg",$group_id,$begin[2]]);
-      }*/
-      
       }
       }
       function plugins_Chat(){
