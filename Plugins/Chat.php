825403ff77c2fc08cfc0ba34baa728edb59cd645
Mukuro <g2744602949@outlook.com>
2022-10-17 01:36:17 +0800

    Add files via upload

--- Plugins/Chat.php
+++ Plugins/Chat.php
@@ -16,6 +16,7 @@ class Chat{
       $this->Rsend("已取消");
       return;
       }else if (!empty($begin1[2])){
+      print_r($begin1);
       $this->send(["send_group_msg",$source,"已建立任务，并将消息发送对应群聊"]);
       $this->send(["send_group_msg",$group_id,"来自群聊[$source]的\r\n[$this->qq]说：\r\n[".$begin1[2]."]\r\nPS：你可以通过跨群聊天功能来回复"]);
       /*$begin = $this->context($begin[2]);
@@ -33,6 +34,7 @@ class Chat{
       $this->Rsend("请在指令后加上群号哦");
       }
       if (preg_match("/^跨群聊天([0-9]+)$/u",$this->msg,$return)){
+      
       $this->join_group($return[1],$this->qun);
       
       }
