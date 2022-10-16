cee3a53e64a363dbffe3edfd19765bf984f28fec
Mukuro <g2744602949@outlook.com>
2022-10-17 01:22:14 +0800

    Add files via upload

--- Plugins/Chat.php
+++ Plugins/Chat.php
@@ -10,7 +10,7 @@ class Chat{
       use Api;
       function join_group(int $group_id,int $source){
       $this->Rsend("你可以在15秒内发送[取消]来退出\r\n或者在建立任务之后发送[取消]\r\n若15秒之内无任何响应，程序将会退出进程");
-      Co::sleep(2);
+    
       $begin1 = $this->context($group_id,$source);
       if ($begin1[2]=="取消"){
       $this->Rsend("已取消");
