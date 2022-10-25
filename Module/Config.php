de300b46314ce5e33c539094a3c16f36e84a984b
Mukuro <g2744602949@outlook.com>
2022-10-25 15:32:46 +0800

    update
    调整

--- Module/Config.php
+++ Module/Config.php
@@ -6,7 +6,7 @@ $Config_data = [
 //gocq的http服务器端口，注意：与Mukuro_Bot配置无关
 "http_port" => 3366,
 //主人QQ，管理机器人
-"qhost" => null,
+"qhost" => 0,
 //版本号
 "SDK" => 116,
 //是否输出完整data
@@ -19,7 +19,6 @@ if (file_exists("config.json") != true) {
 	$Config_data = json_encode($Config_data, JSON_UNESCAPED_UNICODE);
 	file_put_contents("config.json", $Config_data);
 	echo "[notification]：已生成config.json配置文件\n请配置config.json\n若您未查看config.php文件来正确配置，请后续修改config.json\n";
-	exit;
 } else {
 	if (count(json_decode(file_get_contents("config.json"), true)) > count($Config_data) || count(json_decode(file_get_contents("config.json"), true)) < count($Config_data)) {
 		$Config_data = json_encode($Config_data, JSON_UNESCAPED_UNICODE);
@@ -27,14 +26,13 @@ if (file_exists("config.json") != true) {
 		echo "[notification]：Mukuro_Bot检测到配置更改已重新生成配置文件\n";
 	}
 }
-$database = new Swoole\Table(4096);
+$database = new Swoole\Table(4024);
 $database->column("Data", Swoole\Table::TYPE_STRING, 1024);
 $database->column("Group_message", Swoole\Table::TYPE_STRING, 1024);
 $database->create();
 $BOT_Config = json_decode(file_get_contents("config.json"), true);
 //检查配置
-if ($BOT_Config["qhost"] == null) {
-	echo "[notification]：config.json未设置，启动失败。\n";
-	exit;
+if ($BOT_Config["qhost"] == 0) {
+	echo "[notification]：目前无主人状态，现私聊发送[!/Mukuro]即可完成认证\n";
 }
 ?>
