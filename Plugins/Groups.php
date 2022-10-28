7be1bce794f788d699fac95c1b8f7a161ff9d210
Mukuro <g2744602949@outlook.com>
2022-10-28 20:54:58 +0800

    Add files via upload

--- Plugins/Groups.php
+++ Plugins/Groups.php
@@ -13,9 +13,10 @@ class Groups{
       if ($this->super_user == $this->qq){
       $this->Rsend("请发送需要广播的消息");
       //内容是$msg_data[2]
-      $msg_data = $this->context("广播",$this->qun,$this->qq,0);
+      $msg_data = $this->context("广播",$this->qun,$this->qq);
       
       $group_list = File_retrieval("./Group/",true);
+      print_r($group_list);
       Group_Send($group_list,$msg_data[2]);
       
       }
