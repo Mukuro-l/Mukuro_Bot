24f19757bedf3bec38ab4f62b87aaa1ba4619d17
Mukuro <g2744602949@outlook.com>
2022-10-28 21:08:15 +0800

    Add files via upload

--- Plugins/Groups.php
+++ Plugins/Groups.php
@@ -9,15 +9,11 @@ use Mukuro\Module\Api;
 class Groups{
       use Api;
       function plugins_Groups(){
-      if ($this->msg == "广播"){
+      if (preg_match("/^广播?(.*)\$/",$this->msg,$return)){
       if ($this->super_user == $this->qq){
-      $this->Rsend("请发送需要广播的消息");
-      //内容是$msg_data[2]
-      $msg_data = $this->context("广播",$this->qun,$this->qq);
-      
-      $group_list = File_retrieval("./Group/",true);
+      $group_list = $this->File_retrieval("./Group/",true);
       print_r($group_list);
-      Group_Send($group_list,$msg_data[2]);
+      Group_Send($group_list,$return[1]);
       
       }
       
