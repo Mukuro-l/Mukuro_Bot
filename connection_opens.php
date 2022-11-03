5fcb982c063463d2702bafa955fc8f557a275a0f
Mukuro <g2744602949@outlook.com>
2022-11-04 02:08:25 +0800

    Add files via upload

---
+++ connection_opens.php
@@ -0,0 +1,25 @@
+<?php
+include './Module/Config.php';
+    echo "go-cqhttp已连接\n";
+    $Welcome_to_use = "Mukuro已成功启动\n欢迎使用Mukuro\n当前版本：" . $BOT_Config["SDK"] . "\n项目地址：https://github.com/Mukuro-l/Mukuro_Bot\nQQ邮箱：coldeggs@qq.com\nOutlook邮箱：g2744602949@outlook.com\n";
+    echo $Welcome_to_use;
+        $file_array = json_decode(file_get_contents("Plugins_switch.json"), true);
+        $list = glob('./Plugins/*.php');
+                    //判断是否增加
+                    if (count($list) > count($file_array)) {
+                        for ($i = 0;$i < count($list);$i++) {
+                            $file = explode('/', $list[$i]) [2];
+                            if ($file_array[$i]["插件名"] != $file) {
+                                unlink("Plugins_switch.json");
+                            }
+                        }
+                    }
+                    //如果少于
+                    if (count($list) < count($file_array)) {
+                        for ($i = 0;$i < count($list);$i++) {
+                            $file = explode('/', $list[$i]) [2];
+                            if ($file_array[$i]["插件名"] != $file) {
+                                unlink("Plugins_switch.json");
+                            }
+                        }
+                    }
