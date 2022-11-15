c53d151dc46692d50ddd69f9bfbd7040b5e17d56
Mukuro <g2744602949@outlook.com>
2022-11-15 22:24:44 +0800

    Update run.php

--- run.php
+++ run.php
@@ -302,7 +302,7 @@ $ws->on('Receive', function ($ws, $fd, $reactor_id, $task_data) {
 
 
 $ws->on('Task', function ($ws) use ($Data,$database, $BOT_Config,$task_id) {
-print_r($Data);
+var_dump($Data);
 go(function()use ($ws,$Data,$database,$BOT_Config){
     include './vendor/autoload.php';
     include_once './Module/Function.php';
