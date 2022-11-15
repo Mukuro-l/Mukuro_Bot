45b66196454095a549e3b6486ce4e6ffa1e4cca2
Mukuro <g2744602949@outlook.com>
2022-11-15 21:43:53 +0800

    Update run.php

--- run.php
+++ run.php
@@ -301,7 +301,7 @@ $ws->on('Receive', function ($ws, $fd, $reactor_id, $task_data) {
 });
 
 
-$ws->on('Task', function ($ws,$task_id) use ($Data,$database, $BOT_Config,$task_id) {
+$ws->on('Task', function ($ws,$Data) use ($Data,$database, $BOT_Config,$task_id) {
 print_r($Data);
 go(function()use ($ws,$Data,$database,$BOT_Config){
     include './vendor/autoload.php';
