bd46c1af1677f2aa329e20469f516b156d766cc7
Mukuro <g2744602949@outlook.com>
2022-11-16 22:42:42 +0800

    Update Config.php

--- Module/Config.php
+++ Module/Config.php
@@ -26,10 +26,10 @@ if (file_exists("config.json") != true) {
         echo "[notification]：Mukuro_Bot检测到配置更改已重新生成配置文件\n";
     }
 }
-//申请1MB的内存
-$database = new Swoole\Table(1024*1024);
-$database->column("Data", Swoole\Table::TYPE_STRING, (1024*1024)/2);
-$database->column("Group_message", Swoole\Table::TYPE_STRING, (1024*1024)/2);
+//申请6024B的内存
+$database = new Swoole\Table(6024);
+$database->column("Data", Swoole\Table::TYPE_STRING, 3024);
+$database->column("Group_message", Swoole\Table::TYPE_STRING, 3024);
 $database->create();
 $BOT_Config = json_decode(file_get_contents("config.json"), true);
 //检查配置
