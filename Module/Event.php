408de5ce826574bfe2715459cbc830a91864d31d
Mukuro <g2744602949@outlook.com>
2022-11-21 20:04:23 +0800

    Update Event.php

--- Module/Event.php
+++ Module/Event.php
@@ -40,7 +40,7 @@ class Event
                    $qq = $this->qq;
                    $this->send("[CQ:at,qq=".$this->qq."] 请在2分钟之内发送2次[验证]，超时则禁言");
                    Co::sleep(60);
-                   $msg_data=$this->context("验证",$x,$qq,120);
+                   $msg_data=$this->context(0,$x,$qq,120);
                       if ($msg_data[1]=="验证"){
                          $this->send("[CQ:at,qq=".$qq."] 验证成功");
 }else{
